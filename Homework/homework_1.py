de3dba25e5408151d0fa46915534300c7e0cf159
Ryan Ragasa <RJ@ryans-imac.home>
2018-01-30 19:16:22 -0800

    Completed the following defs: fast_complement, remove_interval,kmer_list, kmer_set. 14 left...

---
+++ Homework/homework_1.py
@@ -0,0 +1,242 @@
+"""
+Homework 01
+DO NOT RENAME THIS FILE OR ANY DEFINITIONS!
+Place this file in your github repo inside of a folder titled "Homework".
+"""
+
+
+# String Functions
+def fast_complement(dna):
+    """
+    Uses a dictionary to convert a DNA sequence into the complement strand.  C <--> G,  T <--> A
+    :param dna: a string containing only the characters C, T, A, and G
+    :return: a string containing only the characters C, T, A, and G
+    """
+    complementDna = ""
+    complementDict = {'C':'G', 'G':'C', 'T':'A', 'A': 'T'}
+
+    for base in dna:
+        complementDna += complementDict[base]
+
+    return complementDna
+
+
+def remove_interval(s, start, stop):
+    """
+    Removes the interval of characters from a string or list inclusively, 0 based
+    EX: remove_intervals('ABCDEFGHI', 2, 5) will return 'ABGHI'.
+    :param s: a string
+    :param start: a non-negative integer
+    :param stop: a non-negative integer greater than the start integer.
+    :return: a string
+    """
+    modifiedString = ""
+    modifiedString += s[:start]
+    modifiedString += s[stop+1:]
+
+    return modifiedString
+
+
+def kmer_list(s, k):
+    """
+    Generates all kmers of size k for a string s and store them in a list
+    :param s: any string
+    :param k: any integer greater than zero
+    :return: a list of strings
+    """
+    kmerList = []
+    startingInterval = 0
+
+    while(k <= len(s)):
+        kmerList.append(s[startingInterval:k])
+        startingInterval += 1
+        k += 1
+
+    return kmerList
+
+def kmer_set(s, k):
+    """
+    Generates all kmers of size k for a string s and store them in a set
+    :param s: any string
+    :param k: any integer greater than zero
+    :return: a set of strings
+    """
+    kmerSet = set()
+    startingInterval = 0
+
+    while(k <= len(s)):
+        kmerSet.add(s[startingInterval:k])
+        startingInterval += 1
+        k += 1
+
+    return kmerSet
+
+def kmer_dict(s, k):
+    #The string is: "ATTTY"
+    #k-size is 2
+    #kmerList => "AT, TT, TT, TY"
+    ###When calling => kmer_dict("ATTTY",2) we get...
+    ###kmer_dict=>{AT:1, TT:2, TY:1}
+
+    """
+    Generates all kmers of size k for a string s and store them in a dictionary with the
+    kmer(string) as the key and the number of occurances of the kmer as the value(int).
+    :param s: any string
+    :param k: any integer greater than zero
+    :return: a set of strings
+    """
+    return
+
+# Reading Files
+def head(file_name):
+    """
+    with open('my_file.txt', 'r') as x:
+        for line in x.readlines():
+        print(line[0:9])
+    """
+    """
+    Prints the FIRST 10 lines of a file
+    :param file_name: a string
+    :return: None
+    """
+    return
+
+def tail(file_name):
+    """
+    with open('my_file.txt', 'r') as x:
+        for line in x.readlines():
+        print(line[-10:])
+    """
+    """
+    Prints the LAST 10 lines of a file
+    :param file_name: a string
+    :return: None
+    """
+    return
+
+def print_even(file_name):
+    """
+    lineNumber = 1
+    with open('my_file.txt', 'r') as x:
+        for line in x.readlines():
+            if (lineNumber % 2 == 0)
+                print(line)
+            lineNumber += 1
+    """
+
+    """
+    with open('my_file.txt', 'r') as x:
+        lines = x.read().split('\n')
+        print(lines[1::2])
+    """
+
+    """
+    Prints the even numbered lines of a file
+    :param file_name: a string
+    :return: None
+    """
+    return
+
+def csv_list(file_name):
+    """
+    Read in a CSV file to a 2D array (In python it is a list of lists)
+    :param file_name: a string
+    :return: a list of lists
+    """
+    return
+
+def get_csv_column(file_name, column):
+    """
+    Reads in a CSV file and returns a list of values belonging to the column specified
+    :param file_name: a string
+    :param column: a positive integer
+    :return: a list
+    """
+    return
+
+def fasta_seqs(file_name):
+    """
+    Reads in a FASTA file and returns a list of only the sequences
+    :param file_name: a string
+    :return: a list of strings
+    """
+    return
+
+def fasta_headers(file_name):
+    """
+    Reads in a FASTA file and returns a list of only the headers (Lines that start with ">")
+    :param file_name: a string
+    :return: a list of strings
+    """
+    return
+
+def fasta_dict(file_name):
+    """
+    Reads in a FASTA file and returns a dictionary of the format {header: sequence, ...}, where
+    the sequence headers are keys and the sequence is the value
+    :param file_name: a string
+    :return: a dictionary
+    """
+    return
+
+def fastq_to_fasta(file_name, new_name=None):
+    """
+    Reads in a FASTQ file and writes it to a new FASTA file. This definition should also
+    keep the same file name and change the extension to from .fastq to .fasta if new_name is not specified.
+    EX: fastq_to_fasta('ecoli.fastq') should write to a new file called ecoli.fasta
+    :param file_name: a string
+    :param new_name: a string
+    :return: None
+    """
+    return
+
+# Transcription and Translation
+def reverse_complement(dna):
+    """
+    Returns the strand of DNA that is the reverse complement of the sequence given
+    :param dna: a string containing only the characters C, T, A, and G
+    :return: a string containing only the characters C, T, A, and G
+    """
+    return
+
+def transcribe(dna):
+    """
+    Transcribes a string of DNA into RNA
+    :param dna: a string containing only the characters C, T, A, and G
+    :return: a string containing only the characters C, U, A, and G
+    """
+    return
+
+def translate(rna):
+    """
+    Translates the strand of RNA given into its amino acid composition.
+    DO NOT INCLUDE * IN YOUR RETURN STRING
+    :param rna: a string containing only the characters C, U, A, and G
+    :return: a string containing only the characters G, A, L, M, F, W, K, Q, E, S, P, V, I, C, Y, H, R, N, D, and T
+    """
+    RNA_CODON_TABLE = {"UUU": "F", "UUC": "F", "UUA": "L", "UUG": "L",
+           "UCU": "S", "UCC": "S", "UCA": "S", "UCG": "S",
+           "UAU": "Y", "UAC": "Y", "UAA": "*", "UAG": "*",
+           "UGU": "C", "UGC": "C", "UGA": "*", "UGG": "W",
+           "CUU": "L", "CUC": "L", "CUA": "L", "CUG": "L",
+           "CCU": "P", "CCC": "P", "CCA": "P", "CCG": "P",
+           "CAU": "H", "CAC": "H", "CAA": "Q", "CAG": "Q",
+           "CGU": "R", "CGC": "R", "CGA": "R", "CGG": "R",
+           "AUU": "I", "AUC": "I", "AUA": "I", "AUG": "M",
+           "ACU": "T", "ACC": "T", "ACA": "T", "ACG": "T",
+           "AAU": "N", "AAC": "N", "AAA": "K", "AAG": "K",
+           "AGU": "S", "AGC": "S", "AGA": "R", "AGG": "R",
+           "GUU": "V", "GUC": "V", "GUA": "V", "GUG": "V",
+           "GCU": "A", "GCC": "A", "GCA": "A", "GCG": "A",
+           "GAU": "D", "GAC": "D", "GAA": "E", "GAG": "E",
+           "GGU": "G", "GGC": "G", "GGA": "G", "GGG": "G"}
+    return
+
+def reading_frames(dna):
+    """
+    Generates a list of all 6 possible reading frames for a given strand of DNA
+    For the non-biologists: https://en.wikipedia.org/wiki/Open_reading_frame
+    :param dna: a string containing only the characters C, T, A, and G
+    :return: a list of 6 strings containing only C, T, A, and G
+    """
+    return
